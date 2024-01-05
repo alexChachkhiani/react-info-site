f534457b45b6d6047cb5cd65d3b4d3144ff195c8
alexOrdnas <devfterr@gmail.com>
2024-01-05 14:31:58 +0400

    changed image locations

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactIcon from "../assets/react-icon-small.png"
 
 export default function Navbar(props) {
   return (
@@ -7,7 +8,7 @@ export default function Navbar(props) {
       >
           <img 
               className="nav--logo_icon"
-              src="./react-icon-small.png"
+              src={ReactIcon}
           />
           <h3 className="nav--logo_text">ReactFacts</h3>
           
